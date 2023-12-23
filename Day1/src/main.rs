3038d7f9a5e0f061db3a9dfd7dc012a1946e67e0
Jesse Bartola <jrbartola@gmail.com>
2023-12-22 20:14:47 -0500

    day1 solution implementation

--- Day1/src/main.rs
+++ Day1/src/main.rs
@@ -1,7 +1,101 @@
 use common::filereader;
+use regex::Regex;
+
+fn parse_line_iterative(line: &String) -> u8 {
+    let mut first_digit = None;
+    let mut second_digit = None;
+
+    for c in line.chars() {
+        match c.to_digit(10) {
+            Some(d) => {
+                if first_digit.is_none() {
+                    first_digit = Some(d);
+                } else {
+                    second_digit = Some(d)
+                }
+            }
+            None => {}
+        }
+    }
+
+    if second_digit.is_none() {
+        let digit = first_digit.unwrap() as u8;
+
+        return digit * 10 + digit;
+    }
+
+    return (first_digit.unwrap() * 10 + second_digit.unwrap()) as u8;
+}
+
+fn get_int_for_string(str: String) -> u8 {
+    match str.as_str() {
+        "one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        _ => panic!("Bad string {}", str),
+    }
+}
+
+fn parse_line_regex(line: &String) -> u8 {
+    let one_digit_regex =
+        Regex::new(r"^.*?(\d|one|two|three|four|five|six|seven|eight|nine).*$").unwrap();
+    let two_digits_regex = Regex::new(r"^.*?(\d|one|two|three|four|five|six|seven|eight|nine).*(\d|one|two|three|four|five|six|seven|eight|nine).*$").unwrap();
+
+    let parse_digit = |capture: Option<regex::Match>| {
+        capture
+            .map(|m| m.as_str().to_string())
+            .map(|s| {
+                if s.len() == 1 {
+                    return s.chars().next().unwrap().to_digit(10).unwrap() as u8;
+                } else {
+                    return get_int_for_string(s);
+                }
+            })
+            .unwrap()
+    };
+
+    two_digits_regex.captures(line).map_or_else(
+        || {
+            one_digit_regex.captures(line).map_or_else(
+                || panic!("Invalid input string {}", line),
+                |caps| {
+                    let first_digit = parse_digit(caps.get(1));
+
+                    first_digit * 10 + first_digit
+                },
+            )
+        },
+        |caps| {
+            let first_digit = parse_digit(caps.get(1));
+
+            if caps.get(2).is_none() {
+                first_digit * 10 + first_digit
+            } else {
+                let second_digit = parse_digit(caps.get(2));
+                first_digit * 10 + second_digit
+            }
+        },
+    )
+}
+
+fn solve(lines: Vec<String>) -> u32 {
+    lines
+        .iter()
+        .fold(0, |acc, line| acc + (parse_line_regex(line) as u32))
+}
 
 fn main() {
-    println!("Hello, world!");
-
-    filereader::read_file();
+    match filereader::read_file("./day1/resources/input.txt") {
+        Ok(lines) => {
+            let result = solve(lines);
+            println!("{:?}", result)
+        }
+        Err(e) => panic!("{}", e),
+    }
 }
--- common/src/filereader.rs
+++ common/src/filereader.rs
@@ -1,9 +1,8 @@
-
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader};
 
-pub fn read_file(path: &str) -> io::Result<Vec<Vec<String>>>{
+pub fn read_file(path: &str) -> io::Result<Vec<String>> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
 
@@ -11,10 +10,7 @@ pub fn read_file(path: &str) -> io::Result<Vec<Vec<String>>>{
 
     for line in reader.lines() {
         let line = line?;
-        let elements: Vec<String> = line.split_whitespace() // or any other splitting logic
-            .map(|s| s.to_string())
-            .collect();
-        data.push(elements);
+        data.push(line);
     }
 
     Ok(data)
