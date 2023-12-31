8065cb7f8cbb54138ac2bf4db7bae26e5cd27243
Jesse Bartola <jrbartola@gmail.com>
2023-12-31 03:34:01 -0500

    dijkstras compare

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,5 +1,6 @@
 use common::filereader;
-use std::cmp::{min, Reverse};
+use common::formatting::format_grid;
+use std::cmp::{max, min, Reverse};
 use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
 
 #[derive(Eq, PartialEq, Clone, Hash, Debug, Copy, Ord, PartialOrd)]
@@ -119,8 +120,9 @@ fn get_neighbors(
 }
 
 fn dijkstras(grid: &Vec<Vec<u8>>, start: (usize, usize), goal: (usize, usize)) -> u32 {
-    let mut distances: HashMap<(usize, usize), u32> = HashMap::new();
-    let mut predecessors: HashMap<(usize, usize), (usize, usize)> = HashMap::new();
+    let mut distances: HashMap<(usize, usize, Direction), u32> = HashMap::new();
+    let mut predecessors: HashMap<(usize, usize, Direction), (usize, usize, Direction)> =
+        HashMap::new();
     let mut priority_queue: BinaryHeap<Reverse<(u32, PQueueCoord)>> = BinaryHeap::new();
 
     priority_queue.push(Reverse((
@@ -132,14 +134,6 @@ fn dijkstras(grid: &Vec<Vec<u8>>, start: (usize, usize), goal: (usize, usize)) -
         },
     )));
 
-    for i in 0..grid.len() {
-        for j in 0..grid[i].len() {
-            let dist = if (i, j) != start { u32::MAX } else { 0 };
-
-            distances.insert((i, j), dist);
-        }
-    }
-
     while let Some(Reverse((
         dist,
         PQueueCoord {
@@ -149,7 +143,7 @@ fn dijkstras(grid: &Vec<Vec<u8>>, start: (usize, usize), goal: (usize, usize)) -
         },
     ))) = priority_queue.pop()
     {
-        let curr_dist = *distances.get(&(r, c)).unwrap();
+        let curr_dist = *distances.get(&(r, c, direction)).unwrap_or(&u32::MAX);
         if dist > curr_dist {
             // Outdated; skip
             continue;
@@ -159,19 +153,27 @@ fn dijkstras(grid: &Vec<Vec<u8>>, start: (usize, usize), goal: (usize, usize)) -
 
         for PQueueCoord {
             coords: (neighbor_row, neighbor_col),
-            direction,
+            direction: neighbor_direction,
             path_len,
         } in neighbors
         {
             let curr_dist = dist + grid[neighbor_row][neighbor_col] as u32;
-            if curr_dist < *distances.get(&(neighbor_row, neighbor_col)).unwrap() {
-                distances.insert((neighbor_row, neighbor_col), curr_dist);
-                predecessors.insert((neighbor_row, neighbor_col), (r, c));
+            if curr_dist
+                < *distances
+                    .get(&(neighbor_row, neighbor_col, neighbor_direction))
+                    .unwrap_or(&u32::MAX)
+            {
+                distances.insert((neighbor_row, neighbor_col, neighbor_direction), curr_dist);
+                predecessors.insert(
+                    (neighbor_row, neighbor_col, neighbor_direction),
+                    (r, c, direction),
+                );
+                // println!("Pushing ({neighbor_row}, {neighbor_col}) with cost of {curr_dist}");
                 priority_queue.push(Reverse((
                     curr_dist,
                     PQueueCoord {
                         coords: (neighbor_row, neighbor_col),
-                        direction,
+                        direction: neighbor_direction,
                         path_len,
                     },
                 )))
@@ -179,7 +181,97 @@ fn dijkstras(grid: &Vec<Vec<u8>>, start: (usize, usize), goal: (usize, usize)) -
         }
     }
 
-    *distances.get(&goal).unwrap()
+    let mut curr = (goal.0, goal.1, Direction::Right);
+    let mut cloned_grid: Vec<Vec<char>> = grid
+        .clone()
+        .iter()
+        .map(|r| {
+            r.iter()
+                .map(|&c| char::from_digit(c as u32, 10).unwrap())
+                .collect()
+        })
+        .collect();
+
+    cloned_grid[goal.0][goal.1] = '.';
+
+    while let Some(&(r, c, dir)) = predecessors.get(&curr) {
+        cloned_grid[r][c] = '.';
+        curr = (r, c, dir);
+    }
+
+    // println!("{:?}", distances);
+    println!("{}", format_grid(&cloned_grid));
+
+    let maybe_right_dist = distances.get(&(goal.0, goal.1, Direction::Right));
+    let maybe_down_dist = distances.get(&(goal.0, goal.1, Direction::Down));
+
+    if maybe_down_dist.is_some() && maybe_down_dist.is_some() {
+        return min::<u32>(*maybe_down_dist.unwrap(), *maybe_right_dist.unwrap());
+    } else if maybe_down_dist.is_some() {
+        return *maybe_down_dist.unwrap();
+    } else if maybe_right_dist.is_some() {
+        return *maybe_right_dist.unwrap();
+    }
+
+    unreachable!()
+}
+
+fn dijkstra(grid: &[&[u8]], minstep: isize, maxstep: isize) -> i64 {
+    let mut dists = HashMap::new();
+    let mut predecessors: HashMap<(usize, usize, (isize, isize)), (usize, usize, (isize, isize))> =
+        HashMap::new();
+    let mut q = BinaryHeap::from_iter([(0, (0, 0, (0, 0)))]);
+    while let Some((cost, (r, c, d))) = q.pop() {
+        if (r, c) == (grid.len() - 1, grid[0].len() - 1) {
+            // println!("{:?}", dists);
+            let goal = (grid.len() - 1, grid[0].len() - 1);
+            let mut curr = (goal.0, goal.1, (0, 1));
+            let mut cloned_grid: Vec<Vec<char>> = grid
+                .clone()
+                .iter()
+                .map(|r| r.iter().map(|&c| c as char).collect())
+                .collect();
+
+            cloned_grid[goal.0][goal.1] = '.';
+
+            while let Some(&(r, c, dir)) = predecessors.get(&curr) {
+                println!("Predecessor of ({}, {}) is ({}, {})", curr.0, curr.1, r, c);
+                cloned_grid[r][c] = '.';
+                curr = (r, c, dir);
+            }
+
+            println!("{}", format_grid(&cloned_grid));
+            return -cost;
+        }
+        if dists.get(&(r, c, d)).is_some_and(|&c| -cost > c) {
+            continue;
+        }
+        for (dr, dc) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+            if d == (dr, dc) || d == (-dr, -dc) {
+                continue;
+            }
+            let mut next_cost = -cost;
+            for dist in 1..=maxstep {
+                let rr = (r as isize + dr * dist) as usize;
+                let cc = (c as isize + dc * dist) as usize;
+                if rr >= grid.len() || cc >= grid[0].len() {
+                    break;
+                }
+                next_cost += (grid[rr][cc] - b'0') as i64;
+                if dist < minstep {
+                    continue;
+                }
+                let key = (rr, cc, (dr, dc));
+                if next_cost < *dists.get(&key).unwrap_or(&i64::MAX) {
+                    // println!("Pushing ({rr}, {cc}) with cost of {next_cost}");
+                    dists.insert(key, next_cost);
+                    predecessors.insert(key, (r, c, d));
+                    q.push((-next_cost, key));
+                }
+            }
+        }
+    }
+    unreachable!()
 }
 
 fn solve(lines: Vec<String>) -> u32 {
@@ -192,7 +284,15 @@ fn solve(lines: Vec<String>) -> u32 {
         })
         .collect();
 
-    dijkstras(&grid, (0, 0), (grid.len() - 1, grid[0].len() - 1))
+    let grid_bytes: &[&[u8]] = &lines.iter().map(|s| str::as_bytes(s)).collect::<Vec<_>>();
+
+    let expected = dijkstra(&grid_bytes, 1, 3);
+    println!("-----------------------------------");
+    let actual = dijkstras(&grid, (0, 0), (grid.len() - 1, grid[0].len() - 1));
+
+    println!("Expected: {}; Actual: {}", expected, actual);
+
+    actual
 }
 
 fn main() {
