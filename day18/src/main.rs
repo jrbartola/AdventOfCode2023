cab249353d214569a8e38a9e24e16c8a1d7e1ee0
Jesse Bartola <jrbartola@gmail.com>
2024-01-07 03:26:11 -0500

    day18 solution

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -2,9 +2,7 @@ use common::filereader;
 use common::filewriter::write_file;
 use common::formatting::format_grid;
 use regex::Regex;
-use std::cmp::{max, min};
-use std::collections::{HashSet, VecDeque};
-use std::ops::Add;
+use std::ops::{Add, Div, Sub};
 
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Copy, Clone)]
 enum Direction {
@@ -45,108 +43,35 @@ impl Direction {
     }
 }
 
-fn fill_bfs(grid: &mut Vec<Vec<char>>, start_coords: Vec<(usize, usize)>) {
-    let mut visited: HashSet<(usize, usize)> = HashSet::new();
-    let mut queue = VecDeque::new();
-
-    queue.extend(start_coords);
-
-    while let Some((r, c)) = queue.pop_front() {
-        if visited.contains(&(r, c)) {
-            continue;
-        }
-
-        let curr_char = grid[r][c];
-
-        if curr_char == '.' {
-            grid[r][c] = 'X';
-        } else {
-            continue;
-        }
-
-        for (r_new, c_new) in get_neighbors(grid, (r, c)) {
-            if grid[r_new][c_new] != '#' && !visited.contains(&(r_new, c_new)) {
-                queue.push_back((r_new, c_new));
-            }
-        }
-    }
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Copy, Clone)]
+struct Point {
+    x: i64,
+    y: i64,
 }
 
-fn get_neighbors(grid: &Vec<Vec<char>>, coords: (usize, usize)) -> Vec<(usize, usize)> {
-    let (r, c) = coords;
-    // let cell = self.get_cell(coords.0, coords.1);
-    let mut neighbors = Vec::with_capacity(8);
-
-    if r > 0 {
-        // Top
-        neighbors.push((r - 1, c));
+impl Point {
+    fn new(x: i64, y: i64) -> Self {
+        Point { x, y }
     }
 
-    if c > 0 {
-        // Left
-        neighbors.push((r, c - 1));
+    fn move_to(&self, direction: Direction, steps: u64) -> Point {
+        let (dir_row, dir_col) = direction.to_unit_coord();
+
+        Point {
+            x: self.x + (dir_row as i64 * steps as i64),
+            y: self.y + (dir_col as i64 * steps as i64),
+        }
     }
 
-    if r < grid.len() - 1 {
-        // Bottom
-        neighbors.push((r + 1, c));
+    fn get_area(points: Vec<Point>) -> u64 {
+        let mut area = 0_i64;
+        for i in 0..points.len() {
+            let curr = points[i];
+            let next = points[(i + 1) % points.len()];
+            area += (curr.y * next.x) - (curr.x * next.y);
+        }
+        return area.abs().div(2) as u64;
     }
-
-    if c < grid[0].len() - 1 {
-        // Right
-        neighbors.push((r, c + 1));
-    }
-
-    neighbors
-}
-
-fn get_border_coords(grid: &Vec<Vec<char>>) -> Vec<(usize, usize)> {
-    let rows = grid.len();
-    let cols = grid[0].len();
-
-    (0..rows)
-        .map(|r| (r, 0))
-        .chain((0..rows).map(|r| (r, cols - 1)))
-        .chain((0..cols).map(|c| (0, c)))
-        .chain((0..cols).map(|c| (rows - 1, c)))
-        .collect()
-}
-
-fn determine_bounding_coords(
-    instructions: &Vec<(Direction, u64)>,
-) -> ((usize, usize), impl Fn((isize, isize)) -> (usize, usize)) {
-    let mut row: isize = 0;
-    let mut col: isize = 0;
-
-    let mut min_row: isize = row;
-    let mut max_row: isize = row;
-
-    let mut min_col: isize = col;
-    let mut max_col: isize = col;
-
-    for &(direction, steps) in instructions {
-        let (r_dir, c_dir) = direction.to_unit_coord();
-
-        row += steps as isize * r_dir;
-        col += steps as isize * c_dir;
-
-        min_row = min(min_row, row);
-        max_row = max(max_row, row);
-
-        min_col = min(min_col, col);
-        max_col = max(max_col, col);
-    }
-
-    let top_left = (min_row, min_col);
-    let bottom_right = (max_row as usize, max_col as usize);
-
-    (
-        (
-            (bottom_right.0 as isize - top_left.0) as usize,
-            (bottom_right.1 as isize - top_left.1) as usize,
-        ),
-        move |(r, c)| ((r - min_row) as usize, (c - min_col) as usize),
-    )
 }
 
 fn parse_line(line: String) -> (Direction, u64) {
@@ -172,101 +97,44 @@ fn parse_line_hex(line: String) -> (Direction, u64) {
     unreachable!()
 }
 
-fn solve(lines: Vec<String>) -> u32 {
+fn solve(lines: Vec<String>) -> u64 {
     let instructions: Vec<_> = lines.into_iter().map(|line| parse_line(line)).collect();
 
-    let ((max_row, max_col), map_to_nonzero_coords) = determine_bounding_coords(&instructions);
+    let mut points = vec![];
+    let mut curr_point = Point::new(0, 0);
 
-    let mut terrain = vec![vec!['.'; max_col + 1]; max_row + 1];
-    let mut vertices = vec![(0, 0)];
-
-    let mut row = 0_isize;
-    let mut col = 0_isize;
-
-    for (direction, steps) in instructions {
-        let (dir_row, dir_col) = direction.to_unit_coord();
-
-        for _ in 0..steps {
-            row += dir_row;
-            col += dir_col;
-
-            let (actual_row, actual_col) = map_to_nonzero_coords((row, col));
-
-            terrain[actual_row][actual_col] = '#';
-        }
-
-        vertices.push(map_to_nonzero_coords((row, col)));
+    for &(direction, steps) in instructions.iter() {
+        points.push(curr_point);
+        curr_point = curr_point.move_to(direction, steps);
     }
 
-    // println!("{}", format_grid(&terrain));
+    points.push(curr_point);
 
-    for coords in get_border_coords(&terrain) {
-        fill_bfs(&mut terrain, vec![coords]);
-    }
+    let border_points = instructions.iter().fold(0, |acc, &(_, count)| acc + count);
+    let inside_area = Point::get_area(points.clone()) - border_points / 2 + 1;
 
-    write_file(
-        "./day18/resources/output.txt",
-        format_grid(&terrain)
-            .split("\n")
-            .map(|s| s.to_owned())
-            .collect(),
-    )
-    .expect("Couldn't write to file for whatever reason");
-
-    (terrain.len() * terrain[0].len()) as u32
-        - terrain
-            .iter()
-            .flatten()
-            .fold(0, |acc, &c| acc + (if c == 'X' { 1 } else { 0 }))
+    border_points + inside_area
 }
 
 fn solve2(lines: Vec<String>) -> u64 {
     let instructions: Vec<_> = lines.into_iter().map(|line| parse_line_hex(line)).collect();
 
-    let ((max_row, max_col), map_to_nonzero_coords) = determine_bounding_coords(&instructions);
-    println!("rows: {}, cols: {}", max_row, max_col);
-    let mut terrain = vec![vec!['.'; max_col + 1]; max_row + 1];
-    let mut vertices = vec![(0, 0)];
+    let mut points = vec![];
+    let mut curr_point = Point::new(0, 0);
 
-    let mut row = 0_isize;
-    let mut col = 0_isize;
-
-    println!("before instructions");
-    for (direction, steps) in instructions {
-        let (dir_row, dir_col) = direction.to_unit_coord();
-
-        for _ in 0..steps {
-            row += dir_row;
-            col += dir_col;
-
-            let (actual_row, actual_col) = map_to_nonzero_coords((row, col));
-
-            terrain[actual_row][actual_col] = '#';
-        }
-
-        vertices.push(map_to_nonzero_coords((row, col)));
+    for &(direction, steps) in instructions.iter() {
+        points.push(curr_point);
+        curr_point = curr_point.move_to(direction, steps);
     }
 
-    println!("rows: {}, cols: {}", terrain.len(), terrain[0].len());
+    points.push(curr_point);
 
-    let border_coords = get_border_coords(&terrain);
+    let border_points = instructions.iter().fold(0, |acc, &(_, count)| acc + count);
+    let inside_area = Point::get_area(points.clone()) - border_points / 2 + 1;
 
-    fill_bfs(&mut terrain, border_coords);
+    println!("border: {:?}, area: {:?}", border_points, inside_area);
 
-    // write_file(
-    //     "./day18/resources/output.txt",
-    //     format_grid(&terrain)
-    //         .split("\n")
-    //         .map(|s| s.to_owned())
-    //         .collect(),
-    // )
-    // .expect("Couldn't write to file for whatever reason");
-
-    (terrain.len() * terrain[0].len()) as u64
-        - terrain
-            .iter()
-            .flatten()
-            .fold(0, |acc, &c| acc + (if c == 'X' { 1 } else { 0 }))
+    border_points + inside_area
 }
 
 fn main() {
