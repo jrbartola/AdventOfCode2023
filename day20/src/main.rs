4e9a7b6dc45efed189f879b3bc9b66c1f7f088e5
Jesse Bartola <jrbartola@gmail.com>
2024-01-11 00:18:47 -0500

    day20 solution

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,15 +1,221 @@
 mod pulse;
 
+use crate::pulse::PulseLevel::{High, Low};
+use crate::pulse::{
+    BroadcasterModule, ConjunctionModule, FlipFlopModule, Mediator, Module, ModuleType,
+    CRUCIAL_MODULES,
+};
 use common::filereader;
+use regex::Regex;
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::thread;
+use std::time::Duration;
+
+fn parse_lines(lines: Vec<String>) -> Vec<Box<dyn Module>> {
+    let line_regex = Regex::new(r"(%|&)?([a-z]+) -> (.*)").unwrap();
+
+    let mut modules: Vec<Box<dyn Module>> = Vec::new();
+    let mut modules_by_sources = HashMap::new();
+
+    for line in lines.iter() {
+        if let Some(captures) = line_regex.captures(line) {
+            let maybe_type = captures.get(1).map(|r| r.as_str());
+            let name = captures.get(2).unwrap().as_str();
+            let destinations = captures
+                .get(3)
+                .unwrap()
+                .as_str()
+                .split(", ")
+                .map(|s| s.to_owned())
+                .collect::<Vec<String>>();
+
+            match maybe_type {
+                Some("%") => {
+                    destinations.iter().for_each(|d| {
+                        modules_by_sources
+                            .entry(d.clone())
+                            .or_insert(vec![])
+                            .push(name)
+                    });
+
+                    modules.push(Box::new(FlipFlopModule::new(
+                        name.to_string(),
+                        destinations,
+                    )));
+                }
+                Some("&") => {
+                    destinations.iter().for_each(|d| {
+                        modules_by_sources
+                            .entry(d.clone())
+                            .or_insert(vec![])
+                            .push(name)
+                    });
+
+                    modules.push(Box::new(ConjunctionModule::new(
+                        name.to_string(),
+                        destinations,
+                        vec![],
+                    )));
+                }
+                None => {
+                    if name != "broadcaster" {
+                        panic!("Only broadcaster should have no module type");
+                    }
+
+                    destinations.iter().for_each(|d| {
+                        modules_by_sources
+                            .entry(d.clone())
+                            .or_insert(vec![])
+                            .push(name)
+                    });
+
+                    modules.push(Box::new(BroadcasterModule::new(
+                        name.to_string(),
+                        destinations,
+                    )));
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    for i in 0..modules.len() {
+        match modules[i].get_type() {
+            ModuleType::Conjunction => {
+                modules[i] = Box::new(ConjunctionModule::new(
+                    modules[i].get_name(),
+                    modules[i].get_destinations().clone(),
+                    modules_by_sources
+                        .get(&modules[i].get_name())
+                        .unwrap()
+                        .clone()
+                        .iter()
+                        .map(|&s| s.to_string())
+                        .collect(),
+                ));
+            }
+            _ => {}
+        }
+    }
+
+    modules
+}
+
+fn print_module_states(modules: &Vec<&Box<dyn Module>>) {
+    let flipflops: Vec<_> = modules
+        .iter()
+        .filter(|m| m.get_type() == ModuleType::FlipFlop)
+        .map(|m| m.as_any().downcast_ref::<FlipFlopModule>().unwrap())
+        .collect();
+
+    let conjunctions: Vec<_> = modules
+        .iter()
+        .filter(|m| m.get_type() == ModuleType::Conjunction)
+        .map(|m| m.as_any().downcast_ref::<ConjunctionModule>().unwrap())
+        .collect();
+
+    flipflops.iter().for_each(|f| println!("{:?}", f));
+    conjunctions.iter().for_each(|c| println!("{:?}", c));
+}
+
+fn print_module_state(modules: &Vec<&Box<dyn Module>>, module: String) {
+    if let Some(matching_module) = modules.iter().find(|m| m.get_name() == module) {
+        match matching_module.get_type() {
+            ModuleType::FlipFlop => {
+                let casted = matching_module
+                    .as_any()
+                    .downcast_ref::<FlipFlopModule>()
+                    .unwrap();
+                println!("{:?}", casted);
+            }
+            ModuleType::Conjunction => {
+                let casted = matching_module
+                    .as_any()
+                    .downcast_ref::<ConjunctionModule>()
+                    .unwrap();
+                println!("{:?}", casted);
+            }
+            ModuleType::Broadcaster => {
+                let casted = matching_module
+                    .as_any()
+                    .downcast_ref::<BroadcasterModule>()
+                    .unwrap();
+                println!("{:?}", casted);
+            }
+        };
+    }
+}
+
+fn find_all_nodes_on_path_to_module(
+    modules: HashMap<String, Box<dyn Module>>,
+    start_module: String,
+    goal_module: String,
+) -> HashSet<String> {
+    let mut relevant: HashSet<String> = HashSet::new();
+    let mut path_stack: VecDeque<(String, Vec<String>)> = VecDeque::new();
+    path_stack.push_back((start_module, Vec::new()));
+
+    while let Some((name, path)) = path_stack.pop_front() {
+        println!("{:?}: {:?}", name, path);
+        if path.contains(&name) {
+            continue;
+        }
+
+        if goal_module == name.as_str() {
+            path.iter().for_each(|p| {
+                relevant.insert(p.clone());
+            });
+
+            relevant.insert(name);
+            continue;
+        }
+
+        for dest in modules.get(&name).unwrap().get_destinations() {
+            let mut updated_path = path.clone();
+            updated_path.push(name.to_owned());
+            path_stack.push_back((dest.to_string(), updated_path));
+        }
+    }
+
+    relevant
+}
 
 fn solve(lines: Vec<String>) -> u32 {
-    23
+    let modules = parse_lines(lines);
+
+    let mut mediator = Mediator::new(modules);
+
+    for _ in 0..1000 {
+        mediator.send_pulse(String::from("broadcaster"), Low);
+    }
+
+    print_module_states(&mediator.modules.values().collect());
+
+    let pulses = mediator.get_pulses();
+
+    pulses.values().product()
+}
+
+fn solve2(lines: Vec<String>) -> u32 {
+    let modules = parse_lines(lines);
+
+    let mut mediator = Mediator::new(modules);
+    let mut i = 1_u64;
+
+    loop {
+        println!("----------Button press {i}----------");
+        mediator.send_pulse(String::from("broadcaster"), Low);
+
+        i += 1;
+    }
+
+    234
 }
 
 fn main() {
     match filereader::read_file("./day20/resources/input.txt") {
         Ok(lines) => {
-            let result = solve(lines);
+            let result = solve2(lines);
             println!("{:?}", result);
         }
         Err(e) => panic!("{}", e),
--- day20/src/pulse.rs
+++ day20/src/pulse.rs
@@ -1,8 +1,13 @@
-use crate::pulse::FlipFlopState::Off;
+use crate::pulse::FlipFlopState::{Off, On};
+use crate::pulse::ModuleType::{Broadcaster, Conjunction, FlipFlop};
 use crate::pulse::PulseLevel::{High, Low};
+use std::any::Any;
 use std::collections::{HashMap, VecDeque};
 
-#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+const BUTTON_SOURCE: &str = "button";
+pub const CRUCIAL_MODULES: [&str; 4] = ["vz", "bq", "qh", "lt"];
+
+#[derive(Debug, Eq, PartialEq, Clone, Copy, Hash)]
 pub(crate) enum PulseLevel {
     High,
     Low,
@@ -11,16 +16,27 @@ pub(crate) enum PulseLevel {
 impl PulseLevel {
     pub fn flip(&self) -> PulseLevel {
         match self {
-            PulseLevel::High => PulseLevel::Low,
-            PulseLevel::Low => PulseLevel::High,
+            High => Low,
+            Low => High,
         }
     }
 }
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub(crate) struct Pulse {
-    level: PulseLevel,
+    source: String,
     destination: String,
+    level: PulseLevel,
+}
+
+impl Pulse {
+    pub fn new(source: String, destination: String, level: PulseLevel) -> Self {
+        Pulse {
+            source,
+            destination,
+            level,
+        }
+    }
 }
 
 #[derive(Debug, Eq, PartialEq, Clone)]
@@ -38,66 +54,202 @@ impl FlipFlopState {
     }
 }
 
-#[derive(Debug, Eq, PartialEq, Clone)]
-pub(crate) enum Module {
-    Broadcaster {
-        name: String,
-        destinations: Vec<String>,
-    },
-    FlipFlop {
-        name: String,
-        destinations: Vec<String>,
-        state: FlipFlopState,
-    },
-    Conjunction {
-        name: String,
-        destinations: Vec<String>,
-        sources: Vec<String>,
-    },
+#[derive(Eq, PartialEq)]
+pub(crate) enum ModuleType {
+    Broadcaster,
+    FlipFlop,
+    Conjunction,
 }
 
-impl Module {
-    pub fn get_name(&self) -> &str {
-        match self {
-            Module::Broadcaster { name, .. } => name,
-            Module::FlipFlop { name, .. } => name,
-            Module::Conjunction { name, .. } => name,
+pub(crate) trait Module: Any {
+    fn as_any(&self) -> &dyn Any;
+
+    fn get_name(&self) -> String;
+
+    fn get_destinations(&self) -> &Vec<String>;
+
+    fn receive_pulse(&mut self, source: String, pulse_level: PulseLevel) -> Vec<Pulse>;
+
+    fn get_type(&self) -> ModuleType;
+}
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub(crate) struct BroadcasterModule {
+    name: String,
+    destinations: Vec<String>,
+}
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub(crate) struct FlipFlopModule {
+    name: String,
+    destinations: Vec<String>,
+    state: FlipFlopState,
+}
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub(crate) struct ConjunctionModule {
+    name: String,
+    destinations: Vec<String>,
+    pub sources: HashMap<String, PulseLevel>,
+    sources_to_pulses: HashMap<String, u64>,
+}
+
+impl BroadcasterModule {
+    pub fn new(name: String, destinations: Vec<String>) -> Self {
+        BroadcasterModule { name, destinations }
+    }
+}
+
+impl Module for BroadcasterModule {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn get_name(&self) -> String {
+        self.name.to_owned()
+    }
+
+    fn get_destinations(&self) -> &Vec<String> {
+        &self.destinations
+    }
+
+    fn receive_pulse(&mut self, source: String, pulse_level: PulseLevel) -> Vec<Pulse> {
+        self.destinations
+            .iter()
+            .map(|d| Pulse::new(self.get_name(), d.to_owned(), pulse_level))
+            .collect()
+    }
+
+    fn get_type(&self) -> ModuleType {
+        Broadcaster
+    }
+}
+
+impl FlipFlopModule {
+    pub fn new(name: String, destinations: Vec<String>) -> Self {
+        FlipFlopModule {
+            name,
+            destinations,
+            state: Off,
         }
     }
 
-    pub fn receive_pulse(&self, pulse_level: PulseLevel) -> Vec<(String, PulseLevel)> {
-        match self {
-            Module::Broadcaster { destinations, .. } => destinations
+    pub fn flip(&mut self) {
+        self.state = self.state.flip();
+    }
+}
+
+impl Module for FlipFlopModule {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn get_name(&self) -> String {
+        self.name.to_owned()
+    }
+
+    fn get_destinations(&self) -> &Vec<String> {
+        &self.destinations
+    }
+
+    fn receive_pulse(&mut self, source: String, pulse_level: PulseLevel) -> Vec<Pulse> {
+        if pulse_level == Low {
+            self.state = self.state.flip();
+
+            self.destinations
                 .iter()
-                .map(|d| (d.to_owned(), pulse_level))
-                .collect(),
-            Module::FlipFlop {
-                state,
-                destinations,
-                ..
-            } => {
-                if pulse_level == Low {
-                    let pulse_level_to_send = if state == Off { High } else { Low };
-                    destinations
-                        .iter()
-                        .map(|d| (d.to_owned(), pulse_level_to_send))
-                        .collect()
-                } else {
-                    Vec::new()
-                }
-            }
-            Module::Conjunction { .. } => {}
+                .map(|d| {
+                    Pulse::new(
+                        self.get_name(),
+                        d.to_owned(),
+                        if self.state == On { High } else { Low },
+                    )
+                })
+                .collect()
+        } else {
+            Vec::new()
         }
+    }
+
+    fn get_type(&self) -> ModuleType {
+        FlipFlop
+    }
+}
+
+impl ConjunctionModule {
+    pub fn new(name: String, destinations: Vec<String>, sources: Vec<String>) -> Self {
+        ConjunctionModule {
+            name,
+            destinations,
+            sources: sources
+                .clone()
+                .into_iter()
+                .fold(HashMap::new(), |mut acc, s| {
+                    acc.insert(s, Low);
+                    acc
+                }),
+            sources_to_pulses: sources.into_iter().fold(HashMap::new(), |mut acc, s| {
+                acc.insert(s, 0);
+                acc
+            }),
+        }
+    }
+}
+
+impl Module for ConjunctionModule {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn get_name(&self) -> String {
+        self.name.to_owned()
+    }
+
+    fn get_destinations(&self) -> &Vec<String> {
+        &self.destinations
+    }
+
+    fn receive_pulse(&mut self, source: String, pulse_level: PulseLevel) -> Vec<Pulse> {
+        self.sources_to_pulses
+            .entry(source.clone())
+            .and_modify(|e| *e += 1);
+
+        // if self.get_name() == "ft" && pulse_level == High
+        // {
+        //     println!(
+        //         "Got a HIGH pulse level from {source}. Full sources: {:?}",
+        //         self.sources
+        //     );
+        // }
+
+        if source != BUTTON_SOURCE {
+            self.sources.insert(source, pulse_level);
+        }
+
+        let pulse_to_send = if self.sources.values().all(|&v| v == High) {
+            Low
+        } else {
+            High
+        };
+
+        self.destinations
+            .iter()
+            .map(|d| Pulse::new(self.get_name(), d.to_owned(), pulse_to_send))
+            .collect()
+    }
+
+    fn get_type(&self) -> ModuleType {
+        Conjunction
     }
 }
 
 pub(crate) struct Mediator {
-    modules: HashMap<String, Module>,
+    pub modules: HashMap<String, Box<dyn Module>>,
     pulses: HashMap<PulseLevel, u32>,
 }
 
 impl Mediator {
-    pub fn new(modules: Vec<Module>) -> Self {
+    pub fn new(modules: Vec<Box<dyn Module>>) -> Self {
         Mediator {
             modules: modules.into_iter().fold(HashMap::new(), |mut acc, module| {
                 acc.insert(module.get_name().to_owned(), module);
@@ -109,37 +261,27 @@ impl Mediator {
 
     pub fn send_pulse(&mut self, module: String, pulse_level: PulseLevel) {
         let mut pulse_queue = VecDeque::new();
-        pulse_queue.push_back((module, pulse_level));
 
-        while let Some((module, pulse_level)) = pulse_queue.pop_front() {
-            let module_obj = self.modules.get(module.as_str()).unwrap();
+        pulse_queue.push_back(Pulse::new(
+            String::from(BUTTON_SOURCE),
+            module.clone(),
+            pulse_level,
+        ));
 
-            match module_obj {
-                Module::Broadcaster { destinations, .. } => {
-                    for dest in destinations {
-                        pulse_queue.push_back((dest.to_owned(), pulse_level));
-                    }
+        while let Some(Pulse {
+            source,
+            destination,
+            level,
+        }) = pulse_queue.pop_front()
+        {
+            *self.pulses.entry(level).or_insert(0) += 1;
+            // It's possible for the destination module not to exist in our mappings. Just ignore it
+            if let Some(module_obj) = self.modules.get_mut(destination.as_str()) {
+                let outbound_pulses = module_obj.receive_pulse(source, level);
+
+                for pulse in outbound_pulses {
+                    pulse_queue.push_back(pulse)
                 }
-                Module::FlipFlop {
-                    name,
-                    destinations,
-                    state,
-                } => {
-                    if pulse_level == Low {
-                        self.modules.insert(
-                            module,
-                            Module::FlipFlop {
-                                name: name.to_owned(),
-                                destinations: destinations.clone(),
-                                state: state.flip(),
-                            },
-                        )
-                    }
-                }
-                Module::Conjunction {
-                    destinations,
-                    sources,
-                } => {}
             }
         }
     }
